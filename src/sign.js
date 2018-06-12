da40f4d0dce92f7396f3ba3eaba1f3d6935095f8
zhangtailin <zhangtailin@ziggurat.cn>
2018-06-12 15:10:30 +0800

    add protobufjs data

--- src/sign.js
+++ src/sign.js
@@ -2,14 +2,19 @@
  * Created by zhangtailin on 2018/6/11.
  */
 
-var _ccProto = require('../protos/chaincode_pb');
+// var _ccProto = require('../protos/chaincode_pb');
+// var textEncoding = require('text-encoding');
+// var textEncoder = new textEncoding.TextEncoder("utf-8");
+// var textDecoder = new textEncoding.TextDecoder("utf-8");
 var ethUtils = require('ethereumjs-util');
+var ProtoBuf = require('protobufjs');
 var settingsConfig = require('./config');
-var Long = require('long');
-var textEncoding = require('text-encoding');
-var textEncoder = new textEncoding.TextEncoder("utf-8");
-var textDecoder = new textEncoding.TextDecoder("utf-8");
 var Wallet = require('./wallet').Wallet;
+var Long = require('long');
+
+var builder = ProtoBuf.loadProtoFile("protos/chaincode.proto");
+var protos = builder.build("protos");
+var SignContent = protos.SignContent;
 
 var createAccount = function () {
     Wallet.generate();
@@ -18,37 +23,36 @@ var createAccount = function () {
 
 var addressFromPrivateKey = function (prikey) {
     var address = ethUtils.privateToAddress(Buffer.from(prikey, "hex"));
-    return address.toString("hex");
+    return settingsConfig.AddressPrefix + address.toString("hex");
 };
 
 var signTX = function (ccId, fcn, arg, msg, counter, inkLimit, priKey) {
     var args = [];
-    args.push(textEncoder.encode(fcn ? fcn : 'invoke'));
-    for (var i = 0; i < arg.length; i++) {
-        args.push(textEncoder.encode(arg[i]));
-    }
-
-    var signContent = new _ccProto.SignContent();
-
-    var senderSpec = new _ccProto.SenderSpec();
     var senderAddress = ethUtils.privateToAddress(new Buffer(priKey, "hex"));
-    senderSpec.setSender(textEncoder.encode(settingsConfig.AddressPrefix + senderAddress.toString("hex")));
-    senderSpec.setCounter(Long.fromString(counter.toString(), true));
-    senderSpec.setInkLimit(textEncoder.encode(inkLimit));
-    senderSpec.setMsg(textEncoder.encode(msg));
-    signContent.setSenderSpec(senderSpec);
+    var senderSpec = {
+        sender: Buffer.from(settingsConfig.AddressPrefix + senderAddress.toString("hex")),
+        counter: Long.fromString(counter.toString()),
+        ink_limit: Buffer.from(inkLimit),
+        msg: Buffer.from(msg)
+    };
+    args.push(Buffer.from(fcn ? fcn : 'invoke', 'utf8'));
+    for (var i=0; i<arg.length; i++) {
+        args.push(Buffer.from(arg[i], 'utf8'));
+    }
+    var invokeSpec = {
+        type: protos.ChaincodeSpec.Type.GOLANG,
+        chaincode_id: {
+            name: ccId
+        },
+        input: {
+            args: args
+        }
+    };
 
-    var invokeSpec = new _ccProto.ChaincodeSpec();
-    invokeSpec.setType(_ccProto.ChaincodeSpec.Type.GOLANG);
-    var chaincde_id = new _ccProto.ChaincodeID();
-    chaincde_id.setName(ccId);
-    invokeSpec.setChaincodeId(chaincde_id);
-    var input = new _ccProto.ChaincodeInput();
-    input.setArgsList(args);
-    invokeSpec.setInput(input);
+    var signContent = new SignContent();
     signContent.setChaincodeSpec(invokeSpec);
-    var bytes = signContent.serializeBinary();
-    var signHash = ethUtils.sha256(textDecoder.decode(bytes));
+    signContent.setSenderSpec(senderSpec);
+    var signHash = ethUtils.sha256(signContent.toBuffer());
     var sigrsv = ethUtils.ecsign(signHash, new Buffer(priKey, "hex"));
 
     return Buffer.concat([
@@ -57,6 +61,44 @@ var signTX = function (ccId, fcn, arg, msg, counter, inkLimit, priKey) {
         ethUtils.toBuffer(sigrsv.v - 27)
     ]).toString("hex");
 };
+
+/* use google-protobuf */
+// var signTX = function (ccId, fcn, arg, msg, counter, inkLimit, priKey) {
+//     var args = [];
+//     args.push(textEncoder.encode(fcn ? fcn : 'invoke'));
+//     for (var i = 0; i < arg.length; i++) {
+//         args.push(textEncoder.encode(arg[i]));
+//     }
+//
+//     var signContent = new _ccProto.SignContent();
+//
+//     var senderSpec = new _ccProto.SenderSpec();
+//     var senderAddress = ethUtils.privateToAddress(new Buffer(priKey, "hex"));
+//     senderSpec.setSender(textEncoder.encode(settingsConfig.AddressPrefix + senderAddress.toString("hex")));
+//     senderSpec.setCounter(Long.fromString(counter.toString(), true));
+//     senderSpec.setInkLimit(textEncoder.encode(inkLimit));
+//     senderSpec.setMsg(textEncoder.encode(msg));
+//     signContent.setSenderSpec(senderSpec);
+//
+//     var invokeSpec = new _ccProto.ChaincodeSpec();
+//     invokeSpec.setType(_ccProto.ChaincodeSpec.Type.GOLANG);
+//     var chaincde_id = new _ccProto.ChaincodeID();
+//     chaincde_id.setName(ccId);
+//     invokeSpec.setChaincodeId(chaincde_id);
+//     var input = new _ccProto.ChaincodeInput();
+//     input.setArgsList(args);
+//     invokeSpec.setInput(input);
+//     signContent.setChaincodeSpec(invokeSpec);
+//     var bytes = signContent.serializeBinary();
+//     var signHash = ethUtils.sha256(textDecoder.decode(bytes));
+//     var sigrsv = ethUtils.ecsign(signHash, new Buffer(priKey, "hex"));
+//
+//     return Buffer.concat([
+//         ethUtils.setLengthLeft(sigrsv.r, 32),
+//         ethUtils.setLengthLeft(sigrsv.s, 32),
+//         ethUtils.toBuffer(sigrsv.v - 27)
+//     ]).toString("hex");
+// };
 
 module.exports.createAccount = createAccount;
 module.exports.addressFromPrivateKey = addressFromPrivateKey;
